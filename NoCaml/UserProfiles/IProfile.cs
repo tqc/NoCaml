35218419295af7c15304024ed178692ca7ef4819
Tom Clarkson <tom@orangeguava.com>
2011-12-07 14:11:22 +1100

    profile marker interface

---
+++ NoCaml/UserProfiles/IProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoCaml.UserProfiles
+{
+    public interface IProfile
+    {
+        string LanID { get; set; }
+
+        Dictionary<string, SourceLogEntry> SourceLog { get; set; }
+
+        Dictionary<string, string> HashLog { get; set; }
+
+
+        void SetUpdated(string p, string source);
+        void Save();
+
+
+        // internals
+        bool ImportedPropertyChanged(string p, string newvalue);
+        string GetCurrentSource(string p);
+        List<string> ChangedProperties { get; set; }
+    }
+}
